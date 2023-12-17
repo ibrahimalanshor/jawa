0010536fe80e46780c8182948d0809f4c9e17205
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-12-17 11:22:09 +0700

    select menu

--- Main.java
+++ Main.java
@@ -1,15 +1,43 @@
+import java.util.Scanner;
+
 class Main {
 
-    public static void displayMenus() {
-        String menus[] = {"List all todos", "Add todos"};
+    static String menus[] = {"List all tasks", "Add tasks", "Exit"};
 
+    public static void displayMenus() {
         for (int i = 0; i < menus.length; i++) {
             System.out.printf("%d. %s%n", i + 1, menus[i]);
         }
     }
 
+    public static int selectMenu() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Select menu (by number) = ");
+        
+        int selectedMenuNumber = Integer.parseInt(scanner.nextLine());
+
+        if (selectedMenuNumber < 1 || selectedMenuNumber > menus.length) {
+            throw new NumberFormatException("Invalud menu number (%d)%n");
+        }
+
+        return selectedMenuNumber;
+    }
+
     public static void main(String args[]) {
-        displayMenus();
+        int selectedMenuNumber;        
+        
+        do {
+            displayMenus();
+        
+            try {
+                selectedMenuNumber = selectMenu();
+            } catch (NumberFormatException e) {
+                System.out.println(e.getMessage());
+
+                break;
+            }
+        } while (selectedMenuNumber != 3);
     }
 
 }
