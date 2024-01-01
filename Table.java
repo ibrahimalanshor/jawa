809b1386a0feac8f50eb2bff7f2529a856691c72
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-01-01 14:11:19 +0700

    table

--- Table.java
+++ Table.java
@@ -4,13 +4,20 @@ import java.util.stream.Stream;
 public class Table {
     private String[] columns;
     private int[] columnsWidths;
+    private ArrayList<String[]> rows = new ArrayList<>();
 
     public void setHeaders(String ...headers) {
         this.columns = headers;
         this.columnsWidths = Stream.of(headers).mapToInt(header -> header.length()).toArray();
     }
 
-    public void addRow(String ...rows) {}
+    public void addRow(String ...rows) {
+        this.rows.add(rows);
+
+        for (int i = 0; i < rows.length; i++) {
+            this.columnsWidths[i] = Math.max(this.columnsWidths[i], rows[i].length());
+        }
+    }
 
     public void printLine() {
         System.out.print('+');
@@ -41,10 +48,11 @@ public class Table {
         System.out.print('|');
 
         for (int i = 0; i < this.columns.length; i++) {
-            String column = this.columns[i];
+            String content = this.columns[i];
+            int columnWidth = this.columnsWidths[i];
 
-            System.out.print(' ' + column + ' ');
-            
+            this.printCol(content, columnWidth);
+
             if (i < this.columns.length - 1) {
                 System.out.print('|');
             }
@@ -56,8 +64,42 @@ public class Table {
         this.printLine();
     }
 
+    public void printCol(String content, int columnWidth) {
+        StringBuilder whiteSpace = new StringBuilder();
+        int remainingColumnWidth = columnWidth - content.length();
+        
+        if (remainingColumnWidth > 0) {
+            whiteSpace.append(" ".repeat(remainingColumnWidth));
+        }
+
+        System.out.print(' ' + content + whiteSpace + ' ');
+    }
+
+    public void printRows() {
+        for (String[] row : this.rows) {
+            System.out.print('|');
+
+            for (int i = 0; i < row.length; i++) {
+                String content = row[i];
+                int columnWidth = this.columnsWidths[i];
+
+                this.printCol(content, columnWidth);
+
+                if (i < row.length - 1) {
+                    System.out.print('|');
+                }
+            }
+
+            System.out.print('|');
+            System.out.println();
+        }
+
+        this.printLine();
+    }
+
     public void print() {
         this.printHeader();
+        this.printRows();
     }
 
 }
