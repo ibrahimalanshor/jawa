1f9a9650a375e7382f32db15549682843d6454d2
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-01-21 14:12:27 +0700

    task add

--- com/mokamoka/main/MokamokaApp.java
+++ com/mokamoka/main/MokamokaApp.java
@@ -1,6 +1,8 @@
 package com.mokamoka.main;
 
-import com.mokamoka.core.menu.MenuConsole;
+import java.util.Scanner;
+
+import com.mokamoka.features.Task.Task;
 
 /**
  * MokamokaApp
@@ -8,6 +10,24 @@ import com.mokamoka.core.menu.MenuConsole;
 public class MokamokaApp {
 
     public static void main(String[] args) {
-        MenuConsole.displayAndSelect();
+        Scanner scanner = new Scanner(System.in);
+
+        boolean inserting = true;
+
+        while (inserting) {
+            System.out.print("Insert task name = ");
+
+            Task task = new Task(scanner.nextLine());
+            task.save();
+
+            System.out.print("Insert again (y/n) = ");
+
+            String again = scanner.nextLine();
+
+            inserting = again.toLowerCase().equals("y");
+        }
+
+        System.out.println(Task.count());
     }
+    
 }
